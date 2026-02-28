57b1889c154b42f1a0cf6f91b6414b8a0bab5938
thedevaman <devaman2508@gmail.com>
2026-02-28 09:29:44 +0530

    data fetched on console

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 import express from 'express'
+import { MongoClient } from 'mongodb'
 
+const dbName = 'college'
+const url = "mongodb://localhost:27017"
+
+const client = new MongoClient(url)
+
+async function dbConnection(){
+    await client.connect()
+    const db = client.db(dbName)
+    const collection = db.collection('students')
+
+    const result = await collection.find().toArray()
+
+    console.log(result)
+}
+
+dbConnection()
 const app = express()
 
 app.get('/',(req,res)=>{
