2dfee3b88a48ccd9a7c261fdb0a7a122388b6316
thedevaman <devaman2508@gmail.com>
2026-03-03 09:08:13 +0530

    fetching data from mongodb

--- index.js
+++ index.js
@@ -6,21 +6,19 @@ const url = "mongodb://localhost:27017"
 
 const client = new MongoClient(url)
 
-async function dbConnection(){
-    await client.connect()
+
+const app = express()
+app.set("view engine",'ejs')
+
+app.get('/',async(req,res)=>{
+
+     await client.connect()
     const db = client.db(dbName)
     const collection = db.collection('students')
 
-    const result = await collection.find().toArray()
+    const students = await collection.find().toArray()
 
-    console.log(result)
-}
-
-dbConnection()
-const app = express()
-
-app.get('/',(req,res)=>{
-    res.send("Home Page")
+    res.render('student',{students})
 })
 
 app.listen(3200)
