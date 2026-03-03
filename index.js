0927870005354524db8602a30e26a695acd16170
thedevaman <devaman2508@gmail.com>
2026-03-03 09:23:00 +0530

    fetch in form of api

--- index.js
+++ index.js
@@ -8,7 +8,31 @@ const client = new MongoClient(url)
 
 
 const app = express()
+
 app.set("view engine",'ejs')
+
+client.connect().then((connection)=>{
+   
+   const db = connection.db(dbName);
+
+    app.get('/api',async(req,res)=>{
+
+          const collection = db.collection("students")
+          const students = await collection.find().toArray();
+          res.send(students)
+    })
+   
+    app.get('/ui',async(req,res)=>{
+
+          const collection = db.collection("students")
+          const students = await collection.find().toArray();
+          res.render('student',{students}) //in this first parameter is file name and second is data
+    })
+
+
+})
+
+
 
 app.get('/',async(req,res)=>{
 
